2745ea998f9997b87b11cbb5301fff196c152177
Drew Koszulinski <drew.koszulinski@gmail.com>
2021-09-09 23:04:12 -0600

    fixing more apostrophes

--- components/Bio.js
+++ components/Bio.js
@@ -5,7 +5,7 @@ const Bio = () => {
             <h1 className="heading">Drew Koszulinski</h1>
             <h3 className="heading">UX Designer & Developer</h3>
             <p className="bio-body">Drew Koszulinski is a UX designer & developer focused on consistent, systematic, and pixel-perfect user experiences. The way I see it, job-centered product direction & design is central to gaining a rock-solid userbase. Smash the contact button below to get in touch!</p>
-            <button className="button primary w-full max-w-sm">Let's talk!</button>
+            <button className="button primary w-full max-w-sm">Let&apos;s talk!</button>
         </div>
     )
 }
